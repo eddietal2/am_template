83dd0e870f654b2ee98a2c249cdd624ca56358fc
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-09-01 00:22:04 -0400

    Landing Page Draft Layout Refine

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,49 +12,60 @@
 </script>
 <main>
   <!-- Hero Section -->
-  <section 
-    class="bg-white p-2"> 
-    <div class="container mx-auto">
-    </div>
-    <div class="flex flex-col lg:flex-row flex-wrap"> 
-      <div class="lg:w-1/2 p-4">
-        <h1 class="text-5xl font-bold">Welcome to Pet Care Direct!</h1>
-        <p class="text-lg">Find everything your furry friend needs to live a happy, healthy life.</p>
-        <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="" srcset=""></div>
+  <section class="bg-white px-2 pt-4"> 
+    <div class="container flex mx-auto">
+      <div class="flex"> 
+        <!-- Photo and Welcome Message -->
+        <div class="w-full">
+          <h1 class="text-3xl font-bold">Welcome to Pet Care Direct!</h1>
+          <p class="text-lg">Find everything your furry friend needs to live a happy, healthy life.</p>
+          <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="" srcset=""></div>
       
-        <div class="lg:w-1/2 p-4 bg-gray-200">
-          <p>Call to Action</p>
+        </div>
+        <!-- Call to Action (CTA) -->
+        <div class="flex w-full items-center justify-center">
+          <div class="bg-gray-100 p-8 rounded shadow-md text-center">
+            <p class="text-xl py-2">Call to Action (CTA) message about why you should hit this button.</p>
+            <Button color="dark">CTA Button</Button>
+          </div>
         </div>
     </div>
   </section>
   
   <!-- Categories -->
   <section class="bg-gray-201 container mx-auto px-2 py-8 mt-16">
-      <h2 class="text-5xl font-bold mb-6">Shop by Category</h2>
+      <h2 class="text-3xl font-bold mb-6">Shop by Category</h2>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <div class="bg-white text-center rounded-lg shadow p-4">
-          <h3 class="text-3xl font-semibold mb-2">Food</h3>
+        <div class="bg-white rounded-lg shadow p-4">
+          <p class="text-xl font-semibold mb-2">Food</p>
+          <div class="bg-gray-100 w-full h-40"></div>
         </div>
-        <div class="bg-white text-center rounded-lg shadow p-4">
-          <h3 class="text-3xl font-semibold mb-2">Litter</h3>
+        <div class="bg-white rounded-lg shadow p-4">
+          <p class="text-xl font-semibold mb-2">Litter</p>
+          <div class="bg-gray-100 w-full h-40"></div>
+          <div></div>
         </div>
-        <div class="bg-white text-center rounded-lg shadow p-4">
-          <h3 class="text-3xl font-semibold mb-2">Clearning</h3>
+        <div class="bg-white rounded-lg shadow p-4">
+          <p class="text-xl font-semibold mb-2">Clearning</p>
+          <div class="bg-gray-100 w-full h-40"></div>
+          <div></div>
         </div>
-        <div class="bg-white text-center rounded-lg shadow p-4">
-          <h3 class="text-3xl font-semibold mb-2">Accessories</h3>
+        <div class="bg-white rounded-lg shadow p-4">
+          <p class="text-xl font-semibold mb-2">Accessories</p>
+          <div class="bg-gray-100 w-full h-40"></div>
+          <div></div>
         </div>
       </div>
   </section>
 
   <!-- Products -->
   <section class="bg-gray-301 container mx-auto px-2 py-8 mt-16">
-      <h2 class="text-5xl font-bold mb-6">Featured Products</h2>
+      <h2 class="text-3xl font-bold mb-6">Featured Products</h2>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
           <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
           <div class="p-4">
-            <h3 class="text-xl font-semibold mb-2">Product Name</h3>
+            <p class="text-xl mb-2 text-[#666] font-bold">Product Name</p>
             <p class="text-gray-600 mb-2">Brief product description highlighting key features and benefits</p>
             <div class="flex items-center justify-between">
               <span class="text-lg font-bold text-green-500">$25</span>
@@ -67,7 +78,7 @@
         <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
           <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
           <div class="p-4">
-            <h3 class="text-xl font-semibold mb-2">Product Name</h3>
+            <p class="text-xl mb-2 text-[#666] font-bold">Product Name</p>
             <p class="text-gray-600 mb-2">Brief product description highlighting key features and benefits</p>
             <div class="flex items-center justify-between">
               <span class="text-lg font-bold text-green-500">$25</span>
@@ -80,7 +91,7 @@
         <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
           <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
           <div class="p-4">
-            <h3 class="text-xl font-semibold mb-2">Product Name</h3>
+            <p class="text-xl mb-2 text-[#666] font-bold">Product Name</p>
             <p class="text-gray-600 mb-2">Brief product description highlighting key features and benefits</p>
             <div class="flex items-center justify-between">
               <span class="text-lg font-bold text-green-500">$25</span>
@@ -96,12 +107,12 @@
 
   <!-- Blogs -->
   <section class="bg-gray-401 container mx-auto px-2 py-8 mt-16">
-    <h2 class="text-5xl font-bold mb-6">Featured Blogs</h2>
+    <h2 class="text-3xl font-bold mb-6">Featured Blogs</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
         <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
         <div class="p-4">
-          <h3 class="text-xl font-semibold mb-2">Blog Name</h3>
+          <p class="text-xl mb-2 text-[#666] font-bold">Blog Name</p>
           <p class="text-gray-600 mb-2">Brief blog description highlighting key features and benefits</p>
           <div class="flex items-center justify-between">
             <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -113,7 +124,7 @@
       <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
         <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
         <div class="p-4">
-          <h3 class="text-xl font-semibold mb-2">Blog Name</h3>
+          <p class="text-xl mb-2 text-[#666] font-bold">Blog Name</p>
           <p class="text-gray-600 mb-2">Brief blog description highlighting key features and benefits</p>
           <div class="flex items-center justify-between">
             <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -125,7 +136,7 @@
       <div class="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition duration-300 ease-in-out">
         <img src="https://t3.ftcdn.net/jpg/04/81/85/46/360_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg" alt="Product Name" class="w-full h-48 object-cover"> 
         <div class="p-4">
-          <h3 class="text-xl font-semibold mb-2">Blog Name</h3>
+          <p class="text-xl mb-2 text-[#666] font-bold">Blog Name</p>
           <p class="text-gray-600 mb-2">Brief blog description highlighting key features and benefits</p>
           <div class="flex items-center justify-between">
             <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
