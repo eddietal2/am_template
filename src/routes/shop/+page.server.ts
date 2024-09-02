383421149e83f5467b7e4918d6738bc0de642219
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-09-02 19:12:36 -0400

    Bug Fix?

--- src/routes/shop/+page.server.ts
+++ src/routes/shop/+page.server.ts
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-export const prerender = false;
+export const prerender = true;
 
 export const load: PageServerLoad = async ({ params }) => {
     console.clear();
